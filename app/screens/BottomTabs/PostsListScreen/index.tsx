03e6c0d5199859328a274478c5419d4c06bf98e7
igorzheludkov <500griven@gmail.com>
2023-09-30 21:00:35 +0300

    add post auth check

--- app/screens/BottomTabs/PostsListScreen/index.tsx
+++ app/screens/BottomTabs/PostsListScreen/index.tsx
@@ -1,14 +1,17 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native'
 import PostsCard from 'app/components/PostCard'
+import { useAppSelector } from 'app/store/hooks'
 import { useGetPostsQuery } from 'app/store/modules/api/posts/postsApi'
 import { TRootStack } from 'app/types/INavigation'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, FlatList } from 'react-native'
 import { FAB } from 'react-native-paper'
+import s from './styles'
 
 type Navigation = NavigationProp<TRootStack>
 
 export default function PostsListScreen() {
   const { navigate } = useNavigation<Navigation>()
+  const { isAuthorized } = useAppSelector((state) => state.auth)
   const posts = useGetPostsQuery(null)
 
   return (
@@ -18,23 +21,7 @@ export default function PostsListScreen() {
         renderItem={({ item }) => <PostsCard post={item} />}
         contentContainerStyle={s.contentContainer}
       />
-      <FAB icon='plus' style={s.fab} onPress={() => navigate('CreatePostScreen')} />
+      {isAuthorized && <FAB icon='plus' style={s.fab} onPress={() => navigate('CreatePostScreen')} />}
     </View>
   )
 }
-
-const s = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-  contentContainer: {
-    gap: 10
-  },
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 0
-  }
-})
---
+++ app/screens/BottomTabs/PostsListScreen/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+const s = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  contentContainer: {
+    gap: 10
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0
+  }
+})
+
+export default s
